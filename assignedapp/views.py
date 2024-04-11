a339a87f42ca4be3f0c04912a1b3c3bf0a1fc588
myname <myemail>
2024-04-11 13:36:31 +0300

    second commit to Django

---
+++ assignedapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
+def aboutus(request):
+    template = loader.get_template('aboutus.html')
+    return HttpResponse(template.render())
+def register(request):
+    template = loader.get_template('register.html')
+    return HttpResponse(template.render())
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+# Create your views here.
